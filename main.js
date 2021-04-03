0614af18601f7cd8cd9973117e2feb48e7d0e31f
NanoV11 <77893211+NanoV11@users.noreply.github.com>
2021-04-03 18:59:45 +0400

    final

---
+++ main.js
@@ -0,0 +1,8 @@
+// function open(){
+//     var show = document.getElementsByClassName("Menu");
+//     if (show.style.display === 'none') {
+//         show.style.display = 'block';
+//       } else {
+//         show.style.display = 'none';
+//       }
+//     }
