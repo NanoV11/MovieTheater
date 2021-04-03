c42aafc02b884e67aa9a2e19170753a08ebc8c21
NanoV11 <77893211+NanoV11@users.noreply.github.com>
2021-04-03 20:39:07 +0400

    final

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-// function open(){
-//     var show = document.getElementsByClassName("Menu");
-//     if (show.style.display === 'none') {
-//         show.style.display = 'block';
-//       } else {
-//         show.style.display = 'none';
-//       }
-//     }
+const openMenu = document.querySelector(".openMenu")
+const header = document.querySelector("header");
+const show = document.querySelector("#list");
+const main = document.querySelector("main");
+
+
+openMenu.addEventListener('click',function(){
+    show.classList.toggle("responsive-nav-items")
+    header.classList.toggle("header-background");
+});
+
+main.addEventListener("click",function(){
+    if(show.classList.contains("responsive-nav-items"))
+    {
+        show.classList.remove("responsive-nav-items")
+        header.classList.remove("header-background");
+    }
+});
+
+
